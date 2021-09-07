ea62b0f548ca39ffe49694d179cfda543d734a2c
Dieter Coopman <dieter@deltasolutions.be>
2021-09-07 22:01:34 +0200

    add mini test suite

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,15 @@
 
 namespace Dietercoopman\Smart\Tests;
 
+use Dietercoopman\Smart\Smart;
+
 class ExampleTest extends TestCase
 {
+
+    public function test_img_gets_stream(){
+        $compiler = app(Smart::class);
+        $image = $compiler->parse('<img src="./tests/test.png" smart width="400">');
+        $this->assertStringContainsString("base64",$image);
+        $this->assertStringContainsString("<img",$image);
+    }
 }
