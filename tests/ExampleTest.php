a112f7fd08000f59487da1097549bb205fe1e943
dietercoopman <dietercoopman@users.noreply.github.com>
2021-09-07 20:01:57 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,11 +6,11 @@ use Dietercoopman\Smart\Smart;
 
 class ExampleTest extends TestCase
 {
-
-    public function test_img_gets_stream(){
+    public function test_img_gets_stream()
+    {
         $compiler = app(Smart::class);
         $image = $compiler->parse('<img src="./tests/test.png" smart width="400">');
-        $this->assertStringContainsString("base64",$image);
-        $this->assertStringContainsString("<img",$image);
+        $this->assertStringContainsString("base64", $image);
+        $this->assertStringContainsString("<img", $image);
     }
 }
