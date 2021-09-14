4730e3160eadf90bdb7e2815b9eec2e2c65a0dc5
Dieter Coopman <dieter@deltasolutions.be>
2021-09-14 22:44:31 +0200

    add php blade tags

--- resources/views/components/smart-image.blade.php
+++ resources/views/components/smart-image.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <?php
-    $compiler = app(\Dietercoopman\Smart\Smart::class);
-    echo $compiler->parse('<img src="'.$src.'" smart '.$attributes->merge().'>');
-    ?>
+    @php
+        $compiler = app(\Dietercoopman\Smart\Smart::class);
+        echo $compiler->parse('<img src="'.$src.'" smart '.$attributes->merge().'>');
+    @endphp
 </div>
